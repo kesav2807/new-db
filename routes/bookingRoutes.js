b1456b5d687cc55d6155169279a74e87da5b95d8
kesavan S <143989015+kesav2807@users.noreply.github.com>
2026-04-20 10:25:11 +0530

    Add files via upload

---
+++ routes/bookingRoutes.js
@@ -0,0 +1,10 @@
+const { getAllBookings, createBooking, updateBookingStatus, deleteBooking } = require('../controllers/bookingController');
+
+const router = express.Router();
+
+router.get('/', getAllBookings);
+router.post('/', createBooking);
+router.put('/:id', updateBookingStatus);
+router.delete('/:id', deleteBooking);
+
+module.exports = router;
